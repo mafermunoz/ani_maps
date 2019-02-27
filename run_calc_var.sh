c26049180e3c96593f391f8f99a8873670f4e829
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-27 22:50:03 +0100

    d

---
+++ run_calc_var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NJOBS=1000
+OUTPATH="/beegfs/dampe/users/mmunozsa/anisotripy/maps_calc/"
+for i in {1..NJOBS}
+do
+    OUTF=$OUTPATH"gal_coord_$i.npy"
+    if [ ! -f ${OUTF} ]; then
+	echo $OUTF
+	sbatch submit_calc_var.sh ${NJOBS} ${i}
+    fi
+done
---
+++ submit_calc_var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=rhel6-long
+#SBATCH --ntasks=1
+#SBATCH --mem=20G
+#SBATCH --job-name=ntSelec
+
+
+export HOME=/atlas/users/mmunozsa/
+
+source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
+dampe_init > /dev/null
+
+
+
+python create_real_map_info.py /beegfs/dampe/users/mmunozsa/anisotripy/ntuples_10_25_ani.root ${1}  ${2}
