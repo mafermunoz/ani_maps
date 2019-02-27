40b24a54ec70444b72d0ccefae260df6902b3404
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-27 23:27:33 +0100

    Update maps_functions.py

--- maps_functions.py
+++ maps_functions.py
@@ -1,3 +1,20 @@
+import ROOT
+from ROOT import TFile, TTree,TVector3,TMatrix,TChain
+import astropy
+import healpy
+import numpy as np
+#ROOT.gSystem.Load('libDmpEvent.so')
+import sys
+import os
+import yaml
+import glob
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import copy
+
+
+
 def deg2rad():
     return np.pi/180
 
