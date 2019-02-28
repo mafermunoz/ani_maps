d9790b96589200df9ffa2f16add5b7e78213d75e
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-28 16:32:09 +0100

    Update add_numpy_files.py

--- add_numpy_files.py
+++ add_numpy_files.py
@@ -7,9 +7,10 @@ file_path='../ntuples_10_25_ani_gal_coord_'
 NJOBS=100
 
 for i in range (NJOBS):
+
     if(i==0):
-        f=np.load(file_path+str(i)+".npy")
+        f=np.load(file_path+str(i+1)+".npy")
     else:
-        dummy=np.load(file_path+str(i)+".npy")
+        dummy=np.load(file_path+str(i+1)+".npy")
         f=np.concatenate((f,dummy),axis=1)
         print(f.shape)
