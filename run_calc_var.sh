c3af49bc8deb91eaaf9e95314bb879987a247960
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-28 14:16:30 +0100

    Update run_calc_var.sh

--- run_calc_var.sh
+++ run_calc_var.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NJOBS=1000
+NJOBS=100
 OUTPATH="/beegfs/dampe/users/mmunozsa/anisotripy/maps_calc/ntuples_10_25_ani_"
-for i in {1..1000}
+for i in {1..100}
 do
     OUTF=$OUTPATH"gal_coord_$i.npy"
     if [ ! -f ${OUTF} ]; then
