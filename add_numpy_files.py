23f7209a8b9bd5901f650fa34944ba4a7f82852f
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-28 17:27:50 +0100

    Update add_numpy_files.py

--- add_numpy_files.py
+++ add_numpy_files.py
@@ -3,14 +3,26 @@ import math
 
 
 
-file_path='../ntuples_10_25_ani_gal_coord_'
+file_path='../'
+file_name='ntuples_10_25_ani_gal_coord'
+file_name2='ntuples_10_25_ani_sat_info'
 NJOBS=100
-
+NMAPS=1000
 for i in range (NJOBS):
 
     if(i==0):
-        f=np.load(file_path+str(i+1)+".npy")
+        f=np.load(file_path+file_name+"_"+str(i+1)+".npy")
+        d=np.load(file_path+file_name2+"_"+str(i+1)+".npy")
     else:
-        dummy=np.load(file_path+str(i+1)+".npy")
+        dummy=np.load(file_path+file_name+"_"+str(i+1)+".npy")
+        dummy2=np.load(file_path+file_name2+"_"+str(i+1)+".npy")
         f=np.concatenate((f,dummy),axis=1)
-        print(f.shape)
+        d=np.concatenate((d,dummy2),axis=1)
+        print(d.shape)
+
+np.save(file_path+file_name,f)
+np.save(file_path+file_name2,d)
+
+random_map=[[]for i in range (NMAPS)]
+
+#for i in range (NMAPS):
