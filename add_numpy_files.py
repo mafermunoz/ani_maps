ca3dd284086955932f6da00d296328ed1fc54572
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-28 15:16:50 +0100

    Update add_numpy_files.py

---
+++ add_numpy_files.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+import operator
+import glob
+
+file_path='../ntuples_10_25_ani_gal_coord_*'
+txt=glob.glob(file_path)
+#fermi_data=fits.open('../../Fermi_data/L1810250505202B3DCA7E90_PH06.fits')
+for i,file  in enumerate (txt):
+    if i==0:
+        f=np.load(file)
+        s=len(f)
+    else:
+
+        dummy=np.load(file)
+        f=np.vstack([f,dummy])
+
+print(f.shape)
