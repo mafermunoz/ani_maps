15cb68db5047de571f74806919ad8f40424cfe5f
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-02-28 15:21:18 +0100

    Update add_numpy_files.py

--- add_numpy_files.py
+++ add_numpy_files.py
@@ -7,12 +7,13 @@ file_path='../ntuples_10_25_ani_gal_coord_*'
 txt=glob.glob(file_path)
 #fermi_data=fits.open('../../Fermi_data/L1810250505202B3DCA7E90_PH06.fits')
 for i,file  in enumerate (txt):
+    print(file)
     if i==0:
         f=np.load(file)
         s=len(f)
     else:
 
         dummy=np.load(file)
-        f=np.stack([f,dummy])
+        f=np.vstack((f,dummy))
 
-print(f.shape)
+print(f.shape())
