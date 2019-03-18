8f79ddec5dfb617e13dcda59d91e85bd2c7aa53e
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-18 11:59:52 +0100

    Create dist_tracks.py

---
+++ dist_tracks.py
@@ -0,0 +1,15 @@
+import numpy as np
+import ROOT
+from ROOT import TVector3
+
+a=np.load("ntuples_10_25_ani_sat_info.npy")
+track_tetha=[]
+track_phi=[]
+for i in range (len(a[1])):
+    t=TVector3(a[2][i][0],a[2][i][1],a[2][i][2])
+    track_tetha.append(t.Theta())
+    trac_phi.append(t.Phi())
+
+
+track_info=np.stack((track_phi,track_tetha))
+np.save("ntuples_10_25_ani_track_info.npy",track_info)
